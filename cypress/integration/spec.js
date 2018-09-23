881bf5187d94c102c438a80cc6fa4140f85bc9ab
Andrew Coats <andrew.coats@gmail.com>
2018-09-22 19:04:31 -0600

    add example use of blackout that is not working correctly.

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,21 @@
-describe('page', () => {
-  it('works', () => {})
+/// <reference types="Cypress" />
+
+context('Home', () => {
+  beforeEach(() => {
+    cy.visit('https://bluemodus.com')
+  })
+
+
+  it('Nabigation item count', () => {
+    cy.get('body').find('.headerMain-navMainLink')
+      .should(($p) => {
+        expect($p).to.have.length(6)
+      });
+
+  })
+
+  it('Backout', () => {
+    cy.screenshot({ capture: 'viewport', blackout: ['.headerMain-navMainLink'] });
+  })
+
 })
